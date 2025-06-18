73c3aa4f833521d7dac12ae8c65fe42e0ff3e8a1
pkumv1 <45692629+pkumv1@users.noreply.github.com>
2025-06-18 17:08:06 +0530

    Create App.js component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TfLDashboard from './components/TfLDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TfLDashboard />
+    </div>
+  );
+}
+
+export default App;
